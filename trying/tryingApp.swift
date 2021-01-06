436312443bd21dad6545c00f4e8859a302775c8e
Josh Nelson <jshnelson4@gmail.com>
2021-01-06 11:32:41 -0500

    Initial Commit

---
+++ trying/tryingApp.swift
@@ -0,0 +1,17 @@
+//
+//  tryingApp.swift
+//  trying
+//
+//  Created by Josh Nelson on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct tryingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
